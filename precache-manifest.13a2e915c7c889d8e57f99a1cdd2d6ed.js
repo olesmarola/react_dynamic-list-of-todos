bc1adaccdc55e9e5c30ff0c9ac4692ec43db7580
Oles Marola <oles_pw@ukr.net>
2019-12-19 12:44:27 +0200

    Updates

---
+++ precache-manifest.13a2e915c7c889d8e57f99a1cdd2d6ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245a2aa13c552c2dbccef61d4ddda81c",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "ab7dc8371f721987f9a1",
+    "url": "/react_dynamic-list-of-todos/static/css/main.8d70da8f.chunk.css"
+  },
+  {
+    "revision": "d073f7f2799935d32130",
+    "url": "/react_dynamic-list-of-todos/static/js/2.82451e1c.chunk.js"
+  },
+  {
+    "revision": "ab7dc8371f721987f9a1",
+    "url": "/react_dynamic-list-of-todos/static/js/main.2c966e57.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
